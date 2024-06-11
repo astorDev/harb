94cecea744f11341e9db330b98aaa231d1fac5b3
Egor Tarasov <pavluchenk0@yandex.ru>
2024-06-11 13:42:36 +0400

    harb cli update and docs

--- cli/index.js
+++ cli/index.js
@@ -19,12 +19,13 @@ async function machine() {
     var load = await si.currentLoad()
     var mem = await si.mem()
     var storage = await si.fsSize();
+    var totalStorage = storage.map(s => s.used + s.available).reduce((a, b) => a + b);
     var usedStorage = storage.map(s => s.used).reduce((a, b) => a + b);
     
     logAsJson({
         cpu : resource(load.currentLoad, 100),
         ram : resource(mem.active, mem.total),
-        storage : resource(usedStorage, storage[0].size)
+        storage : resource(usedStorage, totalStorage)
     })
 
     function resource(used, total) {
