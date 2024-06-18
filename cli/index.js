3a57ad572312ca4c29cf2592dc62cc9775149dda
Egor Tarasov <pavluchenk0@yandex.ru>
2024-06-18 11:52:43 +0400

    containers disarray option (#12)

--- cli/index.js
+++ cli/index.js
@@ -13,6 +13,7 @@ program
     .command('containers')
     .description("Retrieves resources utilization by containers")
     .option('--compact', 'No JSON pretty print')
+    .option('--disarray', 'Single lines instead of array')
     .action(containers)
 
 program.parse()
@@ -47,35 +48,44 @@ async function containers(options) {
     stats = statsRaw.split(/\r?\n/).filter(r => r != '').map(r => JSON.parse(r))
     volumes = JSON.parse(df).Volumes
 
-    containers = stats.map(s => {
-        var sic = siContainers.find(c => c.name == s.Name)
-        var cv = volumes.filter(v => sic.mounts.some(m => m.Name == v.Name)).map(v => v.Size)
-        var rx = cv.map(s => {
-            var matches = /^\d*\.?\d*/.exec(s);
-            var unit = s.replace(matches[0], '')
-            var unitBytes = {
-                "MiB" : 1024 * 1024,
-                "MB" : 1000 * 1000,
-                "GiB" : 1024* 1024 * 1024,
-                "GB" : 1000 * 1000 * 1000,
-                "kB" : 1000,
-                "B" : 1
-            }
-
-            var multiplier = unitBytes[unit]
-            return matches[0] * multiplier;
-        })
-
-        return {
-            id : sic.id,
-            name : sic.name,
-            cpu : +s.CPUPerc.replace('%', ''),
-            ram : sic.memUsage,
-            storage : rx.reduce((a, b) => a + b, 0)
+    if (options.disarray) {
+        for (var s of stats) {
+            var result = assembleContainerInformation(s, siContainers, volumes)
+            logAsJson(result, options.compact)
         }
+    }
+    else {
+        var result = stats.map(s => assembleContainerInformation(s, siContainers, volumes))
+        logAsJson(result, options.compact)
+    }
+}
+
+function assembleContainerInformation(s, siContainers, volumes) {
+    var sic = siContainers.find(c => c.name == s.Name)
+    var cv = volumes.filter(v => sic.mounts.some(m => m.Name == v.Name)).map(v => v.Size)
+    var rx = cv.map(s => {
+        var matches = /^\d*\.?\d*/.exec(s);
+        var unit = s.replace(matches[0], '')
+        var unitBytes = {
+            "MiB": 1024 * 1024,
+            "MB": 1000 * 1000,
+            "GiB": 1024 * 1024 * 1024,
+            "GB": 1000 * 1000 * 1000,
+            "kB": 1000,
+            "B": 1
+        }
+
+        var multiplier = unitBytes[unit]
+        return matches[0] * multiplier;
     })
 
-    logAsJson(containers, options.compact)
+    return {
+        id: sic.id,
+        name: sic.name,
+        cpu: +s.CPUPerc.replace('%', ''),
+        ram: sic.memUsage,
+        storage: rx.reduce((a, b) => a + b, 0)
+    }
 }
 
 async function run(cmd) {
