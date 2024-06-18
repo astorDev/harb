50edf4f4f5432b814e4100f8030ca41f2b64cc40
Egor Tarasov <pavluchenk0@yandex.ru>
2024-06-18 12:30:32 +0400

    harb dashboard with containers (#13)

--- pm2/install.sh
+++ pm2/install.sh
@@ -4,6 +4,6 @@ pm2 update
 pm2 delete harb-machine
 pm2 delete harb-containers
 pm2 start harb --no-autorestart --name harb-machine --cron "*/5 * * * * *" -- machine --compact
-pm2 start harb --no-autorestart --name harb-containers --cron "*/30 * * * * *" -- containers --compact
+pm2 start harb --no-autorestart --name harb-containers --cron "*/30 * * * * *" -- containers --compact --disarray
 pm2 save
 pm2 startup
