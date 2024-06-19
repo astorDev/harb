c6dd8237cccef87c9e1de31318582ecbeb22105e
Egor Tarasov <pavluchenk0@yandex.ru>
2024-06-19 12:54:37 +0400

    scripting introduced

---
+++ pm2/elastic/example/.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+curl -s https://raw.githubusercontent.com/astorDev/nice-shell/main/.sh -o /tmp/nice-shell.sh
+source /tmp/nice-shell.sh
+
+docker compose up -d
+cd ../global-dashboard
+
+if [ -z "$TIMER_DURATION" ]; then
+  log "TIMER_DURATION is not set. Using default value of 30 seconds."
+  TIMER_DURATION=30
+fi
+
+echo "Warming up Kibana. Starting $TIMER_DURATION seconds timer."
+
+for ((i=$TIMER_DURATION; i>-1; i--)); do
+  sleep 1
+  printf "\r%02d seconds left" "$i"
+done
+
+export KIBANA_URL=http://localhost:5601
+sh .sh
+cd ../machine-dashboard
+export MACHINE=one
+sh .sh
+export MACHINE=two
+sh .sh
+cd ../example
---
+++ pm2/elastic/global-dashboard/.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -s https://raw.githubusercontent.com/astorDev/nice-shell/main/.sh -o /tmp/nice-shell.sh
+source /tmp/nice-shell.sh
+
+if [ "$KIBANA_URL" == "" ]; then
+    log "KIBANA_URL is empty. Setting to 'http://localhost:5601'"
+    KIBANA_URL="http://localhost:5601"
+fi
+
+export NODE_NO_WARNINGS=1
+httpyac send .http --all --var host=$KIBANA_URL --output short
