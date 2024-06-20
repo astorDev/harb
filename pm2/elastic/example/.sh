8d796d2f6c9b9e688459267306b153f970f38fd8
Egor Tarasov <pavluchenk0@yandex.ru>
2024-06-20 07:59:25 +0400

    dot instead of source

--- pm2/elastic/example/.sh
+++ pm2/elastic/example/.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -s https://raw.githubusercontent.com/astorDev/nice-shell/main/.sh -o /tmp/nice-shell.sh
-source /tmp/nice-shell.sh
+. /tmp/nice-shell.sh
 
 docker compose up -d
 cd ../global-dashboard
